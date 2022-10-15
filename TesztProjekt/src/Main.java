9b001edc87e04804825a3a87849fc6c729104ab8
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2022-10-15 16:55:47 +0300

    Tulcsordulás

--- TesztProjekt/src/Main.java
+++ TesztProjekt/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte a = 122;
+        byte b = 75;
+        byte osszeg = (byte)(a+b);
     }
 }
