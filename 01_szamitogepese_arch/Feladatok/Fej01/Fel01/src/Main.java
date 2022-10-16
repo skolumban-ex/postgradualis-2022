3c1e1c0f111cb568ae282a3ae7d5b0e265d63f0f
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2022-10-16 14:08:13 +0300

    1/2 feladat elkezdve

---
+++ 01_szamitogepese_arch/Feladatok/Fej01/Fel01/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    // 1. Olvass be két egész számot és írd ki a
+    //a. összegüket
+    //b. különbségüket
+    //c. szorzatukat
+    //d. maradékos osztási hányadosukat
+    //e. osztási maradékukat
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
