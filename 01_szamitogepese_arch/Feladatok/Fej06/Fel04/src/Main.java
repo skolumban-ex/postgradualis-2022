3b5ca6e85fd545e713a05f2d7b88b12ffa671360
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2022-10-23 15:15:53 +0300

    Palindrom ellenorzes globalis valtozoval.

--- 01_szamitogepese_arch/Feladatok/Fej06/Fel04/src/Main.java
+++ 01_szamitogepese_arch/Feladatok/Fej06/Fel04/src/Main.java
@@ -1,5 +1,4 @@
 public class Main {
-
     // Rendes palindrom teszt: Írjon programot amely beolvas egy karakterláncot
     // és arról eldönti, hogy palindróm-e (egy karakterlánc akkor palindróm,
     // ha visszafelé olvasva is ugyan az mint előre, pl: Rád rohan a hordár).
@@ -7,11 +6,12 @@ public class Main {
     // se számítsanak.
     // Olyan megoldást adjon, amiben nem használ csak egy karakterláncot,
     // az eredeti bemenetet (újabb karakterláncokba való átalakítás nélkül oldja meg a feladatot).
+
+    private static String szoveg = "a cápa ette apáca.";
+
     public static void main(String[] args) {
 
-        String szoveg = "a cápa ette apáca.";
-
-        boolean palindrom = PalindromE(szoveg);
+        boolean palindrom = PalindromEAGlobalisSzoveg();
 
         if (palindrom)
             System.out.println("Palindorm a: " + szoveg);
@@ -19,26 +19,26 @@ public class Main {
             System.out.println("Nem palindorm a: " + szoveg);
     }
 
-    public static boolean PalindromE(String ellenorzendoSzoveg) {
+    private static boolean PalindromEAGlobalisSzoveg() {
 
         int karPozElsoFeleben = 0;
-        int karPozHatsoFeleben = ellenorzendoSzoveg.length() - 1;
+        int karPozHatsoFeleben = szoveg.length() - 1;
 
         boolean palindrom = true;
         while (karPozElsoFeleben < karPozHatsoFeleben) {
             // ha az elso pozicio nem erdekes karakter
-            if (palindromsagSzempontjabolErdektelenKarakter(ellenorzendoSzoveg.charAt(karPozElsoFeleben))) {
+            if (palindromsagSzempontjabolErdektelenKarakter(szoveg.charAt(karPozElsoFeleben))) {
                 karPozElsoFeleben++;
                 continue;
             }
             // ha a hatso pozicio nem erdekes karakter
-            if (palindromsagSzempontjabolErdektelenKarakter(ellenorzendoSzoveg.charAt(karPozHatsoFeleben))) {
+            if (palindromsagSzempontjabolErdektelenKarakter(szoveg.charAt(karPozHatsoFeleben))) {
                 karPozHatsoFeleben--;
                 continue;
             }
 
             // ha ket karakteren van a ket pozicio, akkor karaktereket kell hasonlitani
-            if (ellenorzendoSzoveg.charAt(karPozElsoFeleben) != ellenorzendoSzoveg.charAt(karPozHatsoFeleben)) {
+            if (szoveg.charAt(karPozElsoFeleben) != szoveg.charAt(karPozHatsoFeleben)) {
                 // nem palindrom
                 palindrom = false;
                 break;
