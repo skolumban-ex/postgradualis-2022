fc346ff57017bc3d6b0241be83e0d36574b1ec99
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2022-11-26 17:37:49 +0200

    Hanoi inicializalva es alap kirajzolva

---
+++ 02_algoritmusok/x09_Hanoi/src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    private static int[] A;
+    private static int[] B;
+    private static int[] C;
+
+    public static void main(String[] args) {
+
+        KezdetiErtekBeallitas(5);
+
+
+    }
+
+    private static void AllapotKirajzolas(){
+        for (int i = A.length-1; i >= 0 ; i--) {
+            // az oszlop i. szintje kiirodik
+        }
+
+        // foldszint
+        System.out.println("_________");
+
+        // oszlopok neve
+    }
+
+    private static void KezdetiErtekBeallitas(int n) {
+        A = TeliOszlopKeszites(n);
+        B = UresOszlopKeszitese(n);
+        C = UresOszlopKeszitese(n);
+    }
+
+    private static int[] UresOszlopKeszitese(int n) {
+        int[] oszlop = new int[n];
+        for (int i = 0; i < oszlop.length; i++) {
+            oszlop[i] = 0;
+        }
+        return oszlop;
+    }
+
+    private static int[] TeliOszlopKeszites(int n) {
+        int[] oszlop = new int[n];
+        for (int i = 0; i < oszlop.length; i++) {
+            oszlop[i] = n - i;
+        }
+        return oszlop;
+    }
+
+}
