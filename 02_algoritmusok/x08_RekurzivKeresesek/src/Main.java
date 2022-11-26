286d019ad9312e3419df77f6e2eb50406ac85b57
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2022-11-26 13:02:48 +0200

    Kereses rekurzivan, O(n) memória igénnyel a rekurzió miatt

--- 02_algoritmusok/x08_RekurzivKeresesek/src/Main.java
+++ 02_algoritmusok/x08_RekurzivKeresesek/src/Main.java
@@ -2,11 +2,35 @@ public class Main {
     public static void main(String[] args) {
         int[] v = {1, 5, 88, 8, 23, -43, 7};
 
-        int poz = Keres(v, 5);
+        int poz = Keres(v, 11);
+    }
+
+    private static int Keres(int[] v, int x) {
+        // 1 * comp (Keres, n)
+        return Keres(v, x, 0);
+    }
+
+    private static int Keres(int[] v, int x, int rangeStartInclusive) {
+        // ha mar kis feladat a kerdes, azt oldjuk meg
+        // 1
+        if (rangeStartInclusive == v.length - 1)
+            return -1;
+
+        // a kisebb megoldasokbol az eredmeny osszeraksas
+        if (v[rangeStartInclusive] == x)
+            // 1
+            return rangeStartInclusive;
+        else {
+            // visszavezetes kisebb feladatok
+            // kisebb feladatok megoldasa
+            // 1 + comp (Keres, n-1)
+            int pos = Keres(v, x, rangeStartInclusive + 1);
+            return pos;
+        }
     }
 
     // tombben hanzadik elem a kert szam
-    private static int Keres(int[] v, int x) {
+    private static int KeresNNegyzetTaron(int[] v, int x) {
         // ha mar kis feladat a kerdes, azt oldjuk meg
         if (v.length == 0)
             return -1;
@@ -22,7 +46,7 @@ public class Main {
             }
 
             // kisebb feladatok megoldasa
-            int indexAVegben = Keres(vVege, x);
+            int indexAVegben = KeresNNegyzetTaron(vVege, x);
 
             if (indexAVegben != -1) {
                 return indexAVegben + 1;
