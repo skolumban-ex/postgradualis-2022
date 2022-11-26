0692b0fcbb0b411c3fd640c7bdaadb24f5c53eb9
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2022-11-26 12:13:55 +0200

    14/1 direktben

--- 02_algoritmusok/Feladatok/Fej14Fel01/src/Main.java
+++ 02_algoritmusok/Feladatok/Fej14Fel01/src/Main.java
@@ -2,12 +2,24 @@ public class Main {
     //Írjon rekurzív algotmust, ami egy tört számot megszoroz egy egész számmal,
     // kizárólag összeadás művelet használatával.
     public static void main(String[] args) {
-        double sz1 = Szorzat(0.3, -5);
+        double sz1 = SzorzatDirektRek(0.3, 5);
         System.out.println(sz1);
     }
 
+    private static double SzorzatDirektRek(double szorzando, int szorzo) {
+        // ha mar kis feladat szorzando kerdes, azt oldjuk meg
+        if (szorzo == 0)
+            return 0;
+
+        if (szorzo > 0)
+            return SzorzatDirektRek(szorzando, szorzo - 1) + szorzando;
+        else
+            return SzorzatDirektRek(szorzando, szorzo + 1) - szorzando;
+
+    }
+
     private static double Szorzat(double szorzando, int szorzo) {
-        if (szorzo < 0){
+        if (szorzo < 0) {
             szorzo = -szorzo;
             szorzando = -szorzando;
         }
