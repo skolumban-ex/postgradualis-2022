645d009a8324e7c29ace96a28315bdf37baf063b
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2022-11-27 15:37:53 +0200

    Hanoi tornyai befejezes.

--- 02_algoritmusok/x09_Hanoi/src/Main.java
+++ 02_algoritmusok/x09_Hanoi/src/Main.java
@@ -5,15 +5,55 @@ public class Main {
 
     public static void main(String[] args) {
 
-        KezdetiErtekBeallitas(5);
+        KezdetiErtekBeallitas(4);
 
         AllapotKirajzolas();
 
-        // pakoljunk mindent at az A rol a C re
+        ArolCrePakol();
+    }
 
+    private static void ArolCrePakol() {
+        Atpakol(A.length, A, C, B);
+    }
+
+    private static void Atpakol(int hanyat, int[] melyikrol, int[] melyikre, int[] seged) {
+        if (hanyat == 1) {
+            FelsoKorongotAtpakol(melyikrol, melyikre);
+        } else {
+
+            Atpakol(hanyat - 1, melyikrol, seged, melyikre);
+            FelsoKorongotAtpakol(melyikrol, melyikre);
+            Atpakol(hanyat - 1, seged, melyikre, melyikrol);
+        }
+    }
+
+    // egyet pakolok at
+    private static void FelsoKorongotAtpakol(int[] melyikrol, int[] melyikre) {
+        // meg kell keresni, hogy ki a felso korong a MELYIKROL oszlopon
+        int legmagasabbKorongHelye = ElsoSzabadHelyMagassaga(melyikrol) - 1;
+        // vegyuk le a korongot
+        int korong = melyikrol[legmagasabbKorongHelye];
+        melyikrol[legmagasabbKorongHelye] = 0;
+
+        // az elso szabad helyre a melyikre oszlopra
+        int elsoSzabadHelyACelon = ElsoSzabadHelyMagassaga(melyikre);
+        melyikre[elsoSzabadHelyACelon] = korong;
+
+        // rajzoljuk ki az allapotot
+        AllapotKirajzolas();
+    }
+
+    private static int ElsoSzabadHelyMagassaga(int[] oszlop) {
+        int elsoSzabadHelyMagassaga = 0;
+        while (elsoSzabadHelyMagassaga < oszlop.length &&
+                oszlop[elsoSzabadHelyMagassaga] != 0) {
+            ++elsoSzabadHelyMagassaga;
+        }
+        return elsoSzabadHelyMagassaga;
     }
 
     private static void AllapotKirajzolas() {
+        System.out.println();
         for (int i = A.length - 1; i >= 0; i--) {
             // az oszlop i. szintje kiirodik
             String szintSzoveg = SzintszovegEloallitas(i);
@@ -38,13 +78,13 @@ public class Main {
     }
 
     private static String SzintszovegKiegesziteseOszloppal(int szint, int[] oszlop) {
-        String szintSzoveg =" ";
+        String szintSzoveg = " ";
         if (oszlop[szint] != 0) {
             szintSzoveg += oszlop[szint];
         } else {
             szintSzoveg += " ";
         }
-        szintSzoveg +=" ";
+        szintSzoveg += " ";
         return szintSzoveg;
     }
 
