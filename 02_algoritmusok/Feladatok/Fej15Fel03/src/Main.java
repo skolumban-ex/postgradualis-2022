2e93a36dfcc85093f2606d5a01125fa631ac0b53
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2022-12-10 15:59:44 +0200

    15/3 repetitiv megoldasok szurese nelkul

--- 02_algoritmusok/Feladatok/Fej15Fel03/src/Main.java
+++ 02_algoritmusok/Feladatok/Fej15Fel03/src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
 
+    //Írjuk ki egy 𝑛 pozitív egész szám összes összegfelbontását,
+    // vagyis az osszes olyan felbontást, amiben pozitív egész számok összegeként előállítható.
+    //Pl.: 4=1+1+1+1=1+1+2=1+3=2+2
     private static int[] aktualisReszmegoldas;
 
     public static void main(String[] args) {
 
         int helyesMegoldasokSzama = 0;
-        KezdetiReszmegoldadBeallitasa(12);
+        KezdetiReszmegoldadBeallitasa(4);
 
         // backtracking iterativan
         int aktualisSzint = 0;
@@ -32,29 +35,51 @@ public class Main {
                 // backtrack
                 // szint lepes lefele
                 --aktualisSzint;
-                aktualisReszmegoldas[aktualisSzint + 1] = -1;
+                aktualisReszmegoldas[aktualisSzint + 1] = 0;
             }
         }
     }
 
     private static void KezdetiReszmegoldadBeallitasa(int n) {
-
+        aktualisReszmegoldas = new int[n];
     }
 
     private static boolean TeljesMegoldas(int aktualisSzint) {
-        return false;
+        int osszeg = 0;
+        for (int rmi : aktualisReszmegoldas) {
+            osszeg += rmi;
+        }
+
+        return (osszeg == aktualisReszmegoldas.length);
     }
 
     private static boolean EldobandoReszmegoldas(int aktualisSzint) {
-        return false;
+        int osszeg = 0;
+        for (int rmi : aktualisReszmegoldas) {
+            osszeg += rmi;
+        }
+
+        return osszeg > aktualisReszmegoldas.length;
     }
 
     private static boolean SzintenBelulUjReszmegoldas(int aktualisSzint) {
+        int osszeg = 0;
+        for (int rmi : aktualisReszmegoldas) {
+            osszeg += rmi;
+        }
+
+        if (osszeg >= aktualisReszmegoldas.length)
+            return false;
+
+        aktualisReszmegoldas[aktualisSzint]++;
         return true;
     }
 
     private static void MegoldasKiirasa(long megoldasiTippekSzama, int[] megoldasTipp) {
         System.out.print("#" + megoldasiTippekSzama + ": ");
-
+        for (int i = 0; i < aktualisReszmegoldas.length-1; ++i){
+            System.out.print(aktualisReszmegoldas[i]+ " + ");
+        }
+        System.out.println( aktualisReszmegoldas[aktualisReszmegoldas.length-1]);
     }
 }
