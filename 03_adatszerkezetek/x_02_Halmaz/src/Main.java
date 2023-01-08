40cf054208bfcf6104ca00d699a6e3d1eae3a32c
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2023-01-08 12:41:13 +0200

    Dinamikus tomb alapu halmaz tipus nevleg, de meg mindig statikus tombot hasznal.

--- 03_adatszerkezetek/x_02_Halmaz/src/Main.java
+++ 03_adatszerkezetek/x_02_Halmaz/src/Main.java
@@ -153,3 +153,173 @@ class StringHalmazStatikusTombbel {
         return elemek;
     }
 }
+
+class StringHalmazDinamikusTombbel {
+    private String[] taroltErtekek;
+
+    public StringHalmazDinamikusTombbel() {
+        taroltErtekek = new String[0];
+    }
+
+    // nem bent levo elem eseten O(n) + O(n + 2)
+    public boolean HozzaAd(String elem) {
+        // O(n)
+        if (BenneVan(elem))
+            return false;
+
+        // O(1)
+        String[] ujTaroltErtekek = new String[this.taroltErtekek.length + 1];
+        // O(n)
+        for (int i = 0; i < this.taroltErtekek.length; i++) {
+            ujTaroltErtekek[i] = this.taroltErtekek[i];
+        }
+        // O(1)
+        ujTaroltErtekek[ujTaroltErtekek.length - 1] = elem;
+        this.taroltErtekek = ujTaroltErtekek;
+        return true;
+    }
+
+    public int Szamossag() {
+        return taroltErtekek.length;
+    }
+
+    public boolean Kivesz(String elem) {
+        if (!BenneVan(elem))
+            return false;
+
+        String[] ujTaroltElemek = new String[taroltErtekek.length - 1];
+        boolean atugrottukeMarAkivevendot = false;
+        for (int i = 0; i < taroltErtekek.length; i++) {
+            if (taroltErtekek[i].equals(elem)) {
+                atugrottukeMarAkivevendot = true;
+                continue;
+            }
+
+            if (!atugrottukeMarAkivevendot)
+                ujTaroltElemek[i] = taroltErtekek[i];
+            else
+                ujTaroltElemek[i - 1] = taroltErtekek[i];
+        }
+        taroltErtekek = ujTaroltElemek;
+        return true;
+    }
+
+    public StringHalmazDinamikusTombbel Kivon(StringHalmazDinamikusTombbel kivonando) {
+        // minden elemet beteszek az aktualis halmazbol
+        // majd kivesszuk a kivonando elemeit
+
+        StringHalmazDinamikusTombbel eredmeny = new StringHalmazDinamikusTombbel();
+        for (int i = 0; i < this.taroltErtekek.length; i++) {
+            eredmeny.HozzaAd(this.taroltErtekek[i]);
+        }
+
+        for (int i = 0; i < kivonando.taroltErtekek.length; i++) {
+            eredmeny.Kivesz(kivonando.taroltErtekek[i]);
+        }
+        return eredmeny;
+    }
+
+    public StringHalmazDinamikusTombbel Egyesit(StringHalmazDinamikusTombbel halmaz) {
+        StringHalmazDinamikusTombbel eredmeny = new StringHalmazDinamikusTombbel();
+
+        // az összes elem megjelenik benne (csak egyszer)
+        // ami legalabb az egyik halmazban benne van
+        // O(n1 negyzet)
+        for (int i = 0; i < this.taroltErtekek.length; i++) {
+            eredmeny.HozzaAd(this.taroltErtekek[i]);
+        }
+
+        // O(n2 negyzet)
+        for (int i = 0; i < halmaz.taroltErtekek.length; i++) {
+            eredmeny.HozzaAd(halmaz.taroltErtekek[i]);
+        }
+
+        return eredmeny;
+    }
+
+    public StringHalmazDinamikusTombbel Metszet(StringHalmazDinamikusTombbel metszendo) {
+        // az eredmeny halmaz elemei a this es a metszendoben is benne kell legyenek
+        // az aktualis halmaz elemeit megnezzuk, hogy benne vannak-e
+        // a metszendoben
+        StringHalmazDinamikusTombbel metszet = new StringHalmazDinamikusTombbel();
+        for (int i = 0; i < this.taroltErtekek.length; i++) {
+            if (metszendo.BenneVan(this.taroltErtekek[i]))
+                metszet.HozzaAd(this.taroltErtekek[i]);
+        }
+        return metszet;
+    }
+
+    public void Urit() {
+        String[] halmaz = new String[0];
+    }
+
+    // O(n)
+    public boolean BenneVan(String elem) {
+        for (int i = 0; i < taroltErtekek.length; i++) {
+            if (taroltErtekek[i].equals(elem)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean uresE() {
+        return Szamossag() == 0;
+    }
+
+    public String[] Ertekek() {
+        String[] elemek = new String[this.taroltErtekek.length];
+        for (int i = 0; i < this.taroltErtekek.length; i++) {
+            elemek[i] = this.taroltErtekek[i];
+        }
+        return elemek;
+    }
+}
+
+class DinStringTomb {
+
+    // tagvaltozok
+    private String[] taroltErtekek;
+
+    // konstruktor
+    public DinStringTomb() {
+        taroltErtekek = new String[0];
+    }
+
+    public int Hossz() {
+        return taroltErtekek.length;
+    }
+
+    public void AdottPozicionErtekadas(int poz, String adat) {
+        taroltErtekek[poz] = adat;
+    }
+
+    public String AdottPozicioLekerdezese(int poz) {
+        return taroltErtekek[poz];
+    }
+
+    public void PoziciorolTorol(int poz) {
+        String[] valtozottErtekek = new String[taroltErtekek.length - 1];
+        for (int i = 0; i < poz; i++) {
+            valtozottErtekek[i] = taroltErtekek[i];
+        }
+        for (int i = poz + 1; i < taroltErtekek.length; i++) {
+            valtozottErtekek[i - 1] = taroltErtekek[i];
+        }
+        taroltErtekek = valtozottErtekek;
+    }
+
+    public void PozicioraBeszur(int poz, String adat) {
+        String[] valtozottErtekek = new String[taroltErtekek.length + 1];
+        for (int i = 0; i < poz; i++) {
+            valtozottErtekek[i] = taroltErtekek[i];
+        }
+
+        valtozottErtekek[poz] = adat;
+
+        for (int i = poz; i < taroltErtekek.length; i++) {
+            valtozottErtekek[i + 1] = taroltErtekek[i];
+        }
+        taroltErtekek = valtozottErtekek;
+    }
+}
