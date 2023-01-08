4108ef3a028bb1968d0719843abbf880564c7826
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2023-01-08 14:41:56 +0200

    Idomeres es DoubleDinTomb2 bevezetese.

--- 03_adatszerkezetek/x_01_DinamikusTomb/src/Main.java
+++ 03_adatszerkezetek/x_01_DinamikusTomb/src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime;
+
 public class Main {
 
     private static int Eletkor = 23;
@@ -5,47 +7,47 @@ public class Main {
     public static void main(String[] args) {
 
         DinDoubleTomb dinTomb = new DinDoubleTomb();
-        DinDoubleTomb dinTomb2 = new DinDoubleTomb();
-        System.out.println("Hossz: " + dinTomb.Hossz());
 
-        dinTomb.PozicioraBeszur(0, 3.4); // [3.4]
-        dinTomb.PozicioraBeszur(0, -2); // [-2, 3.4]
-        dinTomb.PozicioraBeszur(1, 6); // [-2, 6, 3.4]
 
-        System.out.println("A 2-es pozicion levo ertek: " + dinTomb.AdottPozicioLekerdezese(2));
-
-        dinTomb.AdottPozicionErtekadas(2, 100); // [-2, 6, 100]
-
-        System.out.println("A 2-es pozicion levo ertek: " + dinTomb.AdottPozicioLekerdezese(2));
-
-        dinTomb.PoziciorolTorol(0); // [6, 100]
-
-        System.out.println("A 1-es pozicion levo ertek: " + dinTomb.AdottPozicioLekerdezese(1));
+        for (int m = 1000; m < 30000; m = m + 1000) {
+            // O(m*m)
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < m; i++) {
+                dinTomb.PozicioraBeszur(0, i);
+            }
+            long end = System.currentTimeMillis();
+            System.out.println(m + ", " + (end - start) / 1000);
+        }
     }
 }
 
-class DinDoubleTomb {
+class DinDoubleTomb2 {
 
     // tagvaltozok
     private double[] taroltErtekek;
 
     // konstruktor
-    public DinDoubleTomb() {
+    // O(1)
+    public DinDoubleTomb2() {
         taroltErtekek = new double[0];
     }
 
+    // O(1)
     public int Hossz() {
         return taroltErtekek.length;
     }
 
+    // O(1)
     public void AdottPozicionErtekadas(int poz, double adat) {
         taroltErtekek[poz] = adat;
     }
 
+    // O(1)
     public double AdottPozicioLekerdezese(int poz) {
         return taroltErtekek[poz];
     }
 
+    // O(n)
     public void PoziciorolTorol(int poz) {
         double[] valtozottErtekek = new double[taroltErtekek.length - 1];
         for (int i = 0; i < poz; i++) {
@@ -57,6 +59,61 @@ class DinDoubleTomb {
         taroltErtekek = valtozottErtekek;
     }
 
+    // O(n)
+    public void PozicioraBeszur(int poz, double adat) {
+        double[] valtozottErtekek = new double[taroltErtekek.length + 1];
+        for (int i = 0; i < poz; i++) {
+            valtozottErtekek[i] = taroltErtekek[i];
+        }
+
+        valtozottErtekek[poz] = adat;
+
+        for (int i = poz; i < taroltErtekek.length; i++) {
+            valtozottErtekek[i + 1] = taroltErtekek[i];
+        }
+        taroltErtekek = valtozottErtekek;
+    }
+}
+
+class DinDoubleTomb {
+
+    // tagvaltozok
+    private double[] taroltErtekek;
+
+    // konstruktor
+    // O(1)
+    public DinDoubleTomb() {
+        taroltErtekek = new double[0];
+    }
+
+    // O(1)
+    public int Hossz() {
+        return taroltErtekek.length;
+    }
+
+    // O(1)
+    public void AdottPozicionErtekadas(int poz, double adat) {
+        taroltErtekek[poz] = adat;
+    }
+
+    // O(1)
+    public double AdottPozicioLekerdezese(int poz) {
+        return taroltErtekek[poz];
+    }
+
+    // O(n)
+    public void PoziciorolTorol(int poz) {
+        double[] valtozottErtekek = new double[taroltErtekek.length - 1];
+        for (int i = 0; i < poz; i++) {
+            valtozottErtekek[i] = taroltErtekek[i];
+        }
+        for (int i = poz + 1; i < taroltErtekek.length; i++) {
+            valtozottErtekek[i - 1] = taroltErtekek[i];
+        }
+        taroltErtekek = valtozottErtekek;
+    }
+
+    // O(n)
     public void PozicioraBeszur(int poz, double adat) {
         double[] valtozottErtekek = new double[taroltErtekek.length + 1];
         for (int i = 0; i < poz; i++) {
