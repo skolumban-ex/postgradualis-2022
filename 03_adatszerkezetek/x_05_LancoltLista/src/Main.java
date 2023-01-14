d0b8432d291e85471bf08f883ae12d3467d8ff13
Sándor Kolumbán <sandor.kolumban@mirango.hu>
2023-01-14 10:52:43 +0200

    Lancolt lista elso fele

---
+++ 03_adatszerkezetek/x_05_LancoltLista/src/Main.java
@@ -0,0 +1,97 @@
+public class Main {
+    public static void main(String[] args) {
+
+        LancoltLista lista = new LancoltLista();
+
+        lista.ElejereBeszur(2); // 2
+        lista.ElejereBeszur(-5); // -5 2
+        lista.PozicioraBeszur(1, 7); // -5 7 2
+        lista.AdottPozicionErtekadas(0, 0);
+        lista.AdottPozicionErtekadas(1, 1);
+        lista.AdottPozicionErtekadas(2, 2); // 0 1 2
+        lista.PoziciorolTorol(1); // 0 2
+        lista.PoziciorolTorol(1); // 0
+        System.out.println("A lista elso eleme: " + lista.AdottPozicioLekerdezese(0));
+        lista.ElejerolTorol(); // {}
+        System.out.println("A lista hossza: " + lista.Hossz());
+    }
+}
+
+class Lancszem {
+    private Lancszem kovetkezo;
+    private int adat;
+
+    public void SetKovetkezo(Lancszem kovetkezo) {
+        this.kovetkezo = kovetkezo;
+    }
+
+    public Lancszem GetKovetkezo() {
+        return this.kovetkezo;
+    }
+
+    public void SetAdat(int adat) {
+        this.adat = adat;
+    }
+
+    public int GetAdat() {
+        return adat;
+    }
+}
+
+class LancoltLista {
+    private Lancszem elso;
+
+    public LancoltLista() {
+        this.elso = null;
+    }
+
+    public int Hossz() {
+        return 0;
+    }
+
+    public void AdottPozicionErtekadas(int poz, int adat) {
+        Lancszem adottPozicionLevoSzem = this.elso;
+        for (int i = 0; i < poz; i++) {
+            adottPozicionLevoSzem = adottPozicionLevoSzem.GetKovetkezo();
+        }
+        adottPozicionLevoSzem.SetAdat(adat);
+    }
+
+    public double AdottPozicioLekerdezese(int poz) {
+        return 0;
+    }
+
+    public void ElejerolTorol() {
+
+    }
+
+    public void PoziciorolTorol(int poz) {
+
+    }
+
+    public void ElejereBeszur(int adat) {
+        Lancszem ujElem = new Lancszem();
+        ujElem.SetAdat(adat);
+
+        ujElem.SetKovetkezo(this.elso);
+        this.elso = ujElem;
+    }
+
+    public void PozicioraBeszur(int poz, int adat) {
+        if (poz == 0) {
+            ElejereBeszur(adat);
+        } else {
+            Lancszem ujElem = new Lancszem();
+            ujElem.SetAdat(adat);
+
+            // utolso elem a pozicio elott
+            Lancszem beszurandoElotti = this.elso;
+            for (int i = 1; i < poz; i++) {
+                beszurandoElotti = beszurandoElotti.GetKovetkezo();
+            }
+
+            ujElem.SetKovetkezo(beszurandoElotti.GetKovetkezo());
+            beszurandoElotti.SetKovetkezo(ujElem);
+        }
+    }
+}
